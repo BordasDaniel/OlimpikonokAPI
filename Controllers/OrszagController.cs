e5940d4c276d4cf35bba44cc3d65e58924ddc2d7
Dániel Bordás <bordas.daniel0124@gmail.com>
2025-11-05 11:17:00 +0100

    2 végpont ok...

---
+++ Controllers/OrszagController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OlimpikonokAPI.Models;
+
+namespace OlimpikonokAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrszagController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                using (var context = new OlimpikonokContext())
+                {
+                    List<Orszag> orszagok = context.Orszags.ToList();
+                    return Ok(orszagok);
+                }
+
+            }catch (Exception ex)
+            {
+                List<Orszag> valasz = new();
+                Orszag hiba = new()
+                {
+                    Id = -1,
+                    Nev = $"Hiba a betöltés közben: {ex.Message}",
+                };
+                valasz.Add(hiba);
+                return BadRequest(valasz);
+            }
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            using (var context = new OlimpikonokContext())
+            {
+                try
+                {
+                    Orszag valasz = context.Orszags.FirstOrDefault(o => o.Id == id);
+                    if (valasz != null)
+                    {
+                        return Ok(valasz);
+                    }
+                    else
+                    {
+                        Orszag hiba = new()
+                        {
+                            Id = -1,
+                            Nev = $"Hiba a betöltés közben",
+                        };
+                        return StatusCode(404, hiba);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Orszag hiba = new()
+                    {
+                        Id = -1,
+                        Nev = $"Hiba a betöltés közben: {ex.Message}",
+                    };
+                    return BadRequest(hiba);
+                }
+            }
+        }
+    }
+}
---
+++ Controllers/SportagController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OlimpikonokAPI.Models;
+
+namespace OlimpikonokAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SportagController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                using (var context = new OlimpikonokContext())
+                {
+                    List<Sportag> sportagak = context.Sportags.ToList();
+                    return Ok(sportagak);
+                }
+
+            }catch (Exception ex)
+            {
+                List<Sportag> valasz = new();
+                Sportag hiba = new()
+                {
+                    Id = -1,
+                    Megnevezes = $"Hiba a betöltés közben: {ex.Message}",
+                };
+                valasz.Add(hiba);
+                return BadRequest(valasz);
+            }
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            using (var context = new OlimpikonokContext())
+            {
+                try
+                {
+                    Sportag valasz = context.Sportags.FirstOrDefault(o => o.Id == id);
+                    return Ok(valasz);
+                }
+                catch (Exception ex)
+                {
+                    List<Sportag> valasz = new();
+                    Sportag hiba = new()
+                    {
+                        Id = -1,
+                        Megnevezes = $"Hiba a betöltés közben: {ex.Message}",
+                    };
+                    valasz.Add(hiba);
+                    return BadRequest(valasz);
+
+                }
+            }
+        }
+    }
+}
